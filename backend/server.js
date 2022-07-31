baa24c1261e53671d6d6cc2f24dc8dbc3221b4db
Nigam <patelnigam136@gmail.com>
2022-07-31 22:27:03 +0530

    package

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use(errorHandler);
 app.use(notFound);
 
 //------------------deployement----------------
+
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
